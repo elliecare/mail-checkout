76ccbe70677cdfab19f10ab6320fa8eadccef920
Danilo Prieto <danilo.s.prieto@gmail.com>
2024-03-18 17:04:03 -0300

    [FIX] Re-make path files

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
+const path = require('path');
 
 const templates = {
-    subscription_gold:'./templates/email_subscription_gold',
-    subscription_gold_loan:'./templates/email_subscription_gold_loan',
-    subscription_success:'./templates/subscription_success',
-    email_verification:'./templates/email_verification'
+    subscription_gold:'email_subscription_gold',
+    subscription_gold_loan:'email_subscription_gold_loan',
+    subscription_success:'subscription_success',
+    email_verification:'email_verification'
 }
 
 function getTemplateHTML(selected, params) {
     const archive = templates[selected];
     const {email, password, code, customer_phone} = params;
 
-    const filePath = `${archive}.html`;
-
+    //const filePath = `${archive}.html`;
+    const filePath = path.resolve(__dirname, 'templates', `${archive}.html`);
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             console.error(`Error al leer el archivo ${filePath}: ${err.message}`);
@@ -36,7 +37,7 @@ const test = {
     customer_phone: '+5411664928'
 }
 // Ejemplo de uso
-//getTemplateHTML('subscription_gold_loan', test);
+getTemplateHTML('subscription_gold_loan', test);
 
 module.exports = {
     getTemplateHTML
