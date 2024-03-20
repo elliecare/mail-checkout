05409f07edbe4b7b3464d08d61047e9b59a3400d
Danilo Prieto <danilo.s.prieto@gmail.com>
2024-03-20 11:16:21 -0300

    [FIX] Add first and last name variable

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const path = require('path');
 const templates = {
     subscription_gold:'email_subscription_gold',
     subscription_gold_loan:'email_subscription_gold_loan',
-    subscription_success:'subscription_success',
+    subscription_basic:'email_subscription_basic',
     email_verification:'email_verification'
 }
 
 function getTemplateHTML(selected, params) {
     const archive = templates[selected];
-    const {email, password, code, customer_phone} = params;
+    const {email, password, code, customer_phone, patient_first_name, patient_last_name} = params;
 
     const filePath = path.resolve(__dirname, 'templates', `${archive}.html`);
     fs.readFile(filePath, 'utf8', (err, data) => {
@@ -23,6 +23,8 @@ function getTemplateHTML(selected, params) {
         modifiedHTML = password ? modifiedHTML.replace('%PASSWORD%', password) : modifiedHTML;
         modifiedHTML = code ? modifiedHTML.replace('%CODE%', code) : modifiedHTML;
         modifiedHTML = customer_phone ? modifiedHTML.replace('%CUSTOMER_PHONE%', customer_phone) : modifiedHTML;
+        modifiedHTML = patient_first_name ? modifiedHTML.replace('%PATIENT_FIRST_NAME%', patient_first_name) : modifiedHTML;
+        modifiedHTML = patient_last_name ? modifiedHTML.replace('%PATIENT_LAST_NAME%', patient_last_name) : modifiedHTML;
 
         console.log(modifiedHTML)
         return modifiedHTML;
