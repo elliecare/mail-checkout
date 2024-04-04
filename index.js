4253ffc64da3d5115dbb7dfd17aad22a5b7895dc
Danilo Prieto <danilo.s.prieto@gmail.com>
2024-04-04 14:12:37 -0300

    [FIX] - Add Promise to function and test

--- index.js
+++ index.js
@@ -8,36 +8,41 @@ const templates = {
     email_verification:'email_verification'
 }
 
-function getTemplateHTML(selected, params) {
-    const archive = templates[selected];
-    const {email, password, code, customer_phone, patient_first_name, patient_last_name} = params;
-
-    const filePath = path.resolve(__dirname, 'templates', `${archive}.html`);
-    fs.readFile(filePath, 'utf8', (err, data) => {
-        if (err) {
-            console.error(`Error al leer el archivo ${filePath}: ${err.message}`);
-            return;
-        }
-
-        let modifiedHTML = email ? data.replace('%EMAIL%', email) : data;
-        modifiedHTML = password ? modifiedHTML.replace('%PASSWORD%', password) : modifiedHTML;
-        modifiedHTML = code ? modifiedHTML.replace('%CODE%', code) : modifiedHTML;
-        modifiedHTML = customer_phone ? modifiedHTML.replace('%CUSTOMER_PHONE%', customer_phone) : modifiedHTML;
-        modifiedHTML = patient_first_name ? modifiedHTML.replace('%PATIENT_FIRST_NAME%', patient_first_name) : modifiedHTML;
-        modifiedHTML = patient_last_name ? modifiedHTML.replace('%PATIENT_LAST_NAME%', patient_last_name) : modifiedHTML;
-
-        return modifiedHTML;
-    });
+async function getTemplateHTML(selected, params) {
+    return new Promise((resolve, reject) => {
+        const archive = templates[selected];
+        const {email, password, code, customer_phone, patient_first_name, patient_last_name} = params;
+    
+        const filePath = path.resolve(__dirname, 'templates', `${archive}.html`);
+    
+        fs.readFile(filePath, 'utf8', (err, data) => {
+            if (err) {
+                reject({error:`Error al leer el archivo ${filePath}: ${err.message}`});
+            }
+    
+            let modifiedHTML = email ? data.replace('%EMAIL%', email) : data;
+            modifiedHTML = password ? modifiedHTML.replace('%PASSWORD%', password) : modifiedHTML;
+            modifiedHTML = code ? modifiedHTML.replace('%CODE%', code) : modifiedHTML;
+            modifiedHTML = customer_phone ? modifiedHTML.replace('%CUSTOMER_PHONE%', customer_phone) : modifiedHTML;
+            modifiedHTML = patient_first_name ? modifiedHTML.replace('%PATIENT_FIRST_NAME%', patient_first_name) : modifiedHTML;
+            modifiedHTML = patient_last_name ? modifiedHTML.replace('%PATIENT_LAST_NAME%', patient_last_name) : modifiedHTML;
+    
+            resolve(modifiedHTML);
+        });
+    })
 }
 
-const test = {
-    email:'sarasa@sarasa.com',
-    password: 1234156,
-    sn_code: 48955,
-    customer_phone: '+5411664928'
-}
-// Ejemplo de uso
-//console.log(getTemplateHTML('subscription_gold_loan', test));
+//TEST
+// (async () => {
+//     const test = {
+//         email:'sarasa@sarasa.com',
+//         password: 1234156,
+//         sn_code: 48955,
+//         customer_phone: '+5411664928'
+//     };
+//     const template = await getTemplateHTML('subscription_gold_loan', test);
+//     console.log({template});
+// })();
 
 module.exports = {
     getTemplateHTML
